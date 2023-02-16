2e14ee74125aef784e849710de7f52465730e0d4
Haidar <afiq.haidar12@gmail.com>
2023-02-16 17:29:28 +0700

    reason

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,24 +3,22 @@ import {AiOutlineArrowDown} from 'react-icons/ai'
 
 const Hero = () => {
   return (
-    <section className="bg-[url('/food.jpg')] bg-cover bg-no-repeat " >
-    <div className='w-full h-screen text-center bg-[#00000075] sm:bg-[#00000000] '>
+    <section className="bg-[url('/food2.jpg')] bg-cover bg-no-repeat " >
+    <div className='w-full  text-center bg-[#00000075] md:bg-[#00000000] '>
         <div className='max-w-[1240px] w-full h-full mx-auto p-4 flex justify-center items-center'>
-            <div>
-            <p className='uppercase text-sm tracking-widest text-[#A4C3B2]'>
-                masak tapi garibet?
-            </p>
-            <h1 className='py-4 '>
-                <span className='text-[#A4C3B2]'>Hello</span>Fud!
+            <div className='pt-72'>
+            <h1 className='py-4 font-normal max-w-[60%] m-auto drop-shadow-2xl shadow-white text-black '>
+                Take the stress out of mealtime
             </h1>
-            <p className='py-4 max-w-[50%] m-auto drop-shadow-lg shadow-black text-[#A4C3B2] sm:text-[#6B9080]'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptate id possimus. Vitae, aliquam! Necessitatibus, quasi sapiente. Ipsa amet veritatis ipsam quas! Quas sed aperiam rem assumenda! Itaque, dignissimos esse!
+            <p className='py-4 max-w-[50%] m-auto tracking-widest drop-shadow-lg shadow-black'>
+                recipe made with love
             </p>
             <div className='flex items-center justify-center max-w-[330px] m-auto py-20'>
                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300'>
                     <AiOutlineArrowDown size={20}/>
                 </div>
             </div>
+            
             </div>
         </div>
     </div>
---
+++ src/components/Reason.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import Image from 'next/image';
+
+const Reason = () => {
+  return (
+  <div className='w-full  p-2' >
+    <div className='flex justify-center pt-10 tracking-widest'>
+        <h2 className='font-normal'>Why HelloFood?</h2>
+    </div>
+    <div className='w-full mx-auto flex flex-col justify-center'>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-16 py-16 px-16'>
+
+        <div className='p-6 shadow-xl rounded-xl hover:scale-125 hover:bg-[#CCE3DE] ease-in duration-300'>
+            <div className='grid grid-cols-1 gap-45 justify-center items-center '>
+            <div className='m-auto'>
+                <Image src='/shipped.png' alt='/' width={100} height={100} />
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+                <h3>Fast Shipment</h3>
+                <p>Brings your order as fast as lightning</p>
+            </div>
+            </div>
+        </div>
+
+        <div className='p-6 shadow-xl rounded-xl hover:scale-125 hover:bg-[#CCE3DE] ease-in duration-300'>
+            <div className='grid grid-cols-1 gap-45 justify-center items-center '>
+            <div className='m-auto'>
+                <Image src='/vegetable.png' alt='/' width={100} height={100} />
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+                <h3>Quality Ingredients</h3>
+                <p>Always brings fresh produce daily</p>
+            </div>
+            </div>
+        </div>
+
+        <div className='p-6 shadow-xl rounded-xl hover:scale-125 hover:bg-[#CCE3DE] ease-in duration-300'>
+            <div className='grid grid-cols-1 gap-45 justify-center items-center '>
+            <div className='m-auto'>
+                <Image src='/recipe.png' alt='/' width={100} height={100} />
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+                <h3>Best Recipe</h3>
+                <p>Written by only the best of the best</p>
+            </div>
+            </div>
+        </div>
+
+        <div className='p-6 shadow-xl rounded-xl hover:scale-125 hover:bg-[#CCE3DE] ease-in duration-300'>
+            <div className='grid grid-cols-1 gap-45 justify-center items-center '>
+            <div className='m-auto'>
+                <Image src='/dish.png' alt='/' width={100} height={100} />
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+                <h3>Variety Dishes</h3>
+                <p>Never get bored from the same food</p>
+            </div>
+            </div>
+        </div>
+
+
+        </div>
+    </div>
+  </div>
+  )
+}
+
+export default Reason
